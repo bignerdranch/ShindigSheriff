ce11cacd6292955fa10f6705300e1f3b9ea9d69e
kylie@bignerdranch.com <KylieStradley@Kylies-MacBook-Pro.local>
2014-01-16 00:46:10 -0500

    Updated users relationship to have many events through and organization, allows events to be displayed on users#show. Updated sign-in redirect to users#show to reflect new dashboard [Finishes #62921752]

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ActiveRecord::Base
   has_and_belongs_to_many :organizations
   validates_presence_of :name, :date, :time, :location, :purpose 
+
+   accepts_nested_attributes_for :organizations
 end
 
