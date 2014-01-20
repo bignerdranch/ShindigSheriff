0189a2dbf361bbfc4f10fa9abe59f1483cdbf04e
Chae O'Keefe <chae@bignerdranch.com>
2014-01-20 11:38:05 -0500

    Only finance approver type users are availbale from the selection menu [Finishes #63576156]

--- app/models/finance_approver.rb
+++ app/models/finance_approver.rb
@@ -3,5 +3,11 @@ class FinanceApprover < ActiveRecord::Base
   belongs_to :organization
   
   validates_presence_of :user_id, :organization_id
+
+  def self.fa_users
+    ids = FinanceApprover.pluck(:user_id).uniq.sort
+    binding.pry
+    ids.map {|id| User.find(id)}
+  end
 end
 
