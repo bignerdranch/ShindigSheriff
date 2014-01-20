1f88013a09940662f1753e61ef50131c67730aa7
Chae O'Keefe <chae@bignerdranch.com>
2014-01-20 11:55:20 -0500

    remove unused user files [Fixes #Chore]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,10 @@ class UsersController < ApplicationController
   skip_before_filter :authenticate_user!, only: [:new, :create]
   include UsersHelper
 
-  # GET /users
-  # GET /users.json
   def index
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
     if current_user.finance_approver
       render "finance_show"
@@ -18,20 +14,10 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET /users/new
   def new
-    # if user_signed_in?
-    #   redirect_to user_path(current_user)
-    # end
     @user = User.new
   end
 
-  # GET /users/1/edit
-  def edit
-  end
-
-  # POST /users
-  # POST /users.json
   def create
     @organization = Organization.new(params[:user].delete(:organization))
     @user = User.new(user_params)
@@ -46,30 +32,6 @@ class UsersController < ApplicationController
           #{@user.errors.full_messages.to_sentence} 
           #{@organization.errors.full_messages.to_sentence}"
       redirect_to root_path
-    end
-  end
-
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
     end
   end
 
