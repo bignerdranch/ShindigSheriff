b215236fa43243e963ad75feb7414f0fd5783c31
Chae O'Keefe <chae@bignerdranch.com>
2014-01-24 13:54:08 -0500

    Config ActionMailer [Fixes #63112674]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,20 @@ ShindigSheriff::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # ActionMailer
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              "smtp.gmail.com",
+      port:                 587,
+      domain:               "shindig-sheriff.herokuapp.com",
+      user_name:            "ShindigSheriff@gmail.com",
+      password:             "bignerdranch", 
+      authentication:       "plain",
+      enable_starttles_auto: true }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_options = { from: "ShindigSheriff@gmail.com" }
+  config.action_mailer.raise_delivery_errors = true
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -27,6 +39,4 @@ ShindigSheriff::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  # Devise
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
