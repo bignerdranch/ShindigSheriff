ce6392c23cd0ebac7a53f46b8de49952b987d7fb
Chae O'Keefe <chae@bignerdranch.com>
2014-01-27 12:15:28 -0500

    Add Expenses model associations [Fixes #62929526]

---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,57 @@
+class ExpensesController < ApplicationController
+  include StatusHelper
+
+  def new
+    @event = Event.find(params[:event_id])
+    @income = @event.incomes.new
+  end
+
+  def destroy
+    @income = Income.find(params[:id])
+    @income.delete
+    redirect_to dashboard_path(@user)
+    flash[:notice] =  "Income #{@income.category_details} for $#{@income.estimated_amount} has been deleted"
+  end
+
+  def show
+  end
+
+  def verify
+      @income = Income.find(params[:id])
+
+      @income.update_attribute(:status, true)
+      redirect_to dashboard_path(@user)
+      flash[:notice] =  "Income has been verified"
+  end
+
+   def reject
+      @income = Income.find(params[:id])
+      @income.update_attribute(:status, false)
+      redirect_to dashboard_path(@user)
+      flash[:notice] =  "Income has been rejected"
+  end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @income = Income.new(income_params)
+    
+    if @income.valid? 
+      @event.incomes.build(income_params).save!
+      flash[:notice] = "#{@income.estimated_amount} has successfully been added to organization #{@event.name}!"
+      redirect_to sekret_event_path(@event)
+    else
+      flash[:notice] = "Error(s) while creating income:
+      #{@income.errors.full_messages.to_sentence}"
+      redirect_to new_event_income_path(@event)
+    end
+  end
+
+  private
+
+  def income_params
+    params.require(:income).permit(:estimated_amount, :actual_amount, 
+                                   :date_received, :category_details, :status, :event_id)
+  end
+
+end
+
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ActiveRecord::Base
   has_and_belongs_to_many :organizations
   has_many :incomes
+  has_many :expenses
   has_many :users, through: :organizations
   
   accepts_nested_attributes_for :organizations
