0744f97573acf852eb77f8a9002e45262900ce05
Chae O'Keefe <chae@bignerdranch.com>
2014-01-29 14:45:27 -0500

    Add expenses policy [Fixes #64746828]

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -4,36 +4,40 @@ class ExpensesController < ApplicationController
   def new
     @event = Event.find(params[:event_id])
     @expense = @event.expenses.new
+    authorize @expense
   end
 
   def destroy
     @expense = Expense.find(params[:id])
+    authorize @expense
     @expense.delete
-    redirect_to dashboard_path(@user)
     flash[:notice] =  "expense #{@expense.category_details} for $#{@expense.estimated_amount} has been deleted"
+    redirect_to dashboard_path(@user)
   end
 
   def show
   end
 
   def verify
-      @expense = Expense.find(params[:id])
-
-      @expense.update_attribute(:status, true)
-      redirect_to dashboard_path(@user)
-      flash[:notice] =  "expense has been verified"
+    @expense = Expense.find(params[:id])
+    authorize @expense
+    @expense.update_attribute(:status, true)
+    flash[:notice] =  "expense has been verified"
+    redirect_to dashboard_path(@user)
   end
 
-   def reject
-      @expense = Expense.find(params[:id])
-      @expense.update_attribute(:status, false)
-      redirect_to dashboard_path(@user)
-      flash[:notice] =  "expense has been rejected"
+  def reject
+    @expense = Expense.find(params[:id])
+    authorize @expense
+    @expense.update_attribute(:status, false)
+    flash[:notice] =  "expense has been rejected"
+    redirect_to dashboard_path(@user)
   end
 
   def create
     @event = Event.find(params[:event_id])
     @expense = Expense.new(expense_params)
+    authorize @expense
     
     if @expense.valid? 
       @event.expenses.build(expense_params).save!
