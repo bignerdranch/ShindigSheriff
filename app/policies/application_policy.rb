20cd8f72d3fd218daab2950dbe6d32787d6dce6f
Chae O'Keefe <chae@bignerdranch.com>
2014-01-29 14:30:50 -0500

    Add and config pundit gem [Fixes #64746828]

---
+++ app/policies/application_policy.rb
@@ -0,0 +1,22 @@
+class ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def organizer?
+    user.has_role?("organizer")
+  end
+
+
+  def finance_approver?
+    user.has_role?("finance approver")
+  end
+
+  def verified? 
+    user.verified?
+  end
+end 
+
