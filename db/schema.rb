0febd1abcf4006e0e876b62890a2705725c51cd6
Chae O'Keefe <chae@bignerdranch.com>
2014-01-30 18:23:59 -0500

    Category migration [#63891710]

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140127170517) do
+ActiveRecord::Schema.define(version: 20140130164031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: true do |t|
+    t.string   "name", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "events", force: true do |t|
     t.string   "name"
