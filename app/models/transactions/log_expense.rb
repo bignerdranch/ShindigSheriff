08e09e2d4b6187893067eb3a2898e55d9c244728
Chae O'Keefe <chae@bignerdranch.com>
2014-03-02 14:06:54 -0500

    Provide public and private interface
    [#64823560]

--- app/models/transactions/log_expense.rb
+++ app/models/transactions/log_expense.rb
@@ -8,6 +8,12 @@ module Transactions
       @event_id = params.fetch(:event_id)
     end
 
+    def event_name
+      event.name
+    end
+
+    private
+
     def event
       @event ||= Event.find(@event_id)
     end
