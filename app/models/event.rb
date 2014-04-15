0d319e88f29fddb6041065bc131b58773707bc8c
Chae O'Keefe <chae@bignerdranch.com>
2014-04-15 11:41:09 -0400

    Refactor - group by resource
    [#69540418]

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,8 +3,9 @@ class Event < ActiveRecord::Base
   has_many :organizations, through: :events_organizations
 
   has_many :expenses
-  has_many :users, through: :organizations
+  has_many :incomes
 
+  has_many :users, through: :organizations
   accepts_nested_attributes_for :organizations
 
   validates :date,      presence: true
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,12 +1,12 @@
 class Organization < ActiveRecord::Base
-  has_and_belongs_to_many :events
-  accepts_nested_attributes_for :events
 
   belongs_to :finance_approver, class_name: 'User'
   belongs_to :user
 
   has_many :events_organizations
   has_many :events, through: :events_organizations
+
+  accepts_nested_attributes_for :events
 
   has_many :expenses, through: :events
   has_many :incomes, through: :events
