9eda250a32155d3a7dd41f07c23a7b47c970571b
Chae O'Keefe <chae@bignerdranch.com>
2014-04-15 11:56:53 -0400

    Refactor - Use one feature file per reference, as done elsewhere
    [#69540242]

--- spec/steps/organizations_steps.rb
+++ spec/steps/organizations_steps.rb
@@ -6,3 +6,6 @@ step "I am at the edit organization page" do
   visit edit_organization_path(@organization)
 end
 
+step "I am at the organization page" do
+  visit organization_path(@organization)
+end
