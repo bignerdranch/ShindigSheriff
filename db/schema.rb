b8afe4be9014fb593672eb67188c2f1ee550310b
Chae O'Keefe <chae@bignerdranch.com>
2014-04-16 10:07:25 -0400

    Add db validation for presence
    [#69618172]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140415152813) do
+ActiveRecord::Schema.define(version: 20140416140619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 20140415152813) do
 
   create_table "organizations", force: true do |t|
     t.integer  "user_id"
-    t.string   "name"
+    t.string   "name",                null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "website"
